9c151609651c96a88b64bb2196e966c36364c644
BuzzRage <buzzrage@protonmail.com>
2021-04-24 17:32:20 +0200

    Print socket infos

--- blackholesun.py
+++ blackholesun.py
@@ -1,15 +1,24 @@
 #!/bin/python
-
+import os
+import subprocess
 import requests
 
-print('Download test')
+#url   = 'http://speed.hetzner.de/10GB.bin'
+url   = 'https://eff.org'
+pid   = os.getpid()
 
-url = 'http://speed.hetzner.de/10GB.bin'
+
+print('Download test with PID: ', pid)
+
 r   = requests.get(url, stream=True)
 
 for line in r.iter_lines():
     if r.status_code == 200:
         if line:
             del(line)
-        
+            
+            #Perform 'ss -pti | grep -A 1 pid' request
+            ss = subprocess.Popen(["ss", "-pti"], stdout=subprocess.PIPE)
+            cp = subprocess.run(["grep", "-A 1", str(pid)], stdin=ss.stdout, capture_output=True)
+            print(cp.stdout)
         
