129b89500ab355a73b529372b7edfd4d0ba6300d
BuzzRage <buzzrage@protonmail.com>
2021-04-24 18:04:32 +0200

    Add ss infos each interval of time

--- blackholesun.py
+++ blackholesun.py
@@ -2,23 +2,31 @@
 import os
 import subprocess
 import requests
+import threading
 
-#url   = 'http://speed.hetzner.de/10GB.bin'
-url   = 'https://eff.org'
-pid   = os.getpid()
-
+url     = 'http://speed.hetzner.de/10GB.bin'
+#url     = 'https://eff.org'
+pid     = os.getpid()
+stopped = False
+#Perform 'ss -pti | grep -A 1 pid' request
+def infos():
+    subprocess.run(["clear"])
+    ss = subprocess.Popen(["ss", "-pti"], stdout=subprocess.PIPE)
+    cp = subprocess.run(["grep", "-A 1", str(pid)], stdin=ss.stdout, capture_output=True)
+    print(cp.stdout)
+    if not stopped:
+        threading.Timer(0.4, infos).start()
 
 print('Download test with PID: ', pid)
 
-r   = requests.get(url, stream=True)
+r = requests.get(url, stream=True)
+
+infos()
 
 for line in r.iter_lines():
     if r.status_code == 200:
         if line:
             del(line)
             
-            #Perform 'ss -pti | grep -A 1 pid' request
-            ss = subprocess.Popen(["ss", "-pti"], stdout=subprocess.PIPE)
-            cp = subprocess.run(["grep", "-A 1", str(pid)], stdin=ss.stdout, capture_output=True)
-            print(cp.stdout)
+stopped = True
         
